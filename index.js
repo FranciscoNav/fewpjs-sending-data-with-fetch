bb8f2a798f8508ac422476e3e1ec10e7e32cecb5
Francisco <fnaveira11@gmail.com>
2021-03-28 17:04:11 -0400

    commit tree 39bde5100388e30c8f9fc048ec1f7cc6023f52a2

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function  submitData(userNames, userEmail){
+  
+  const userInfo = {
+    name: userNames, 
+    email: userEmail
+  }
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userInfo)
+  };
+  
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+      return response.json()
+    })
+  . then(function(object) {
+      document.body.innerHTML = object[ "id" ]
+    })
+    .catch(function(error) {
+      alert("This is an error message")
+      document.body.innerHTML = error
+    }); 
+     
+  }
+
+
